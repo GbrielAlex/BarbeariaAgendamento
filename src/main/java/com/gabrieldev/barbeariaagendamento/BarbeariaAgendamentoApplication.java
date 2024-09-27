9130bfb70db7e46ce826b32fdf352db37b1b3611
Gabriel Alexandre da Silva <gbrielti096@gmail.com>
2024-09-27 20:15:43 -0300

    Initial Config

---
+++ src/main/java/com/gabrieldev/barbeariaagendamento/BarbeariaAgendamentoApplication.java
@@ -0,0 +1,13 @@
+package com.gabrieldev.barbeariaagendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaAgendamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarbeariaAgendamentoApplication.class, args);
+    }
+
+}
